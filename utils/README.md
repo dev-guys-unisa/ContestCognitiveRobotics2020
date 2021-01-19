d7f299b4b7e0fbb31ee2e11cf841dff6bf919031
Bettorio <V.FINA@studenti.unisa.it>
2021-01-19 11:34:00 +0100

    Added Noob Section

--- utils/README.md
+++ utils/README.md
@@ -4,6 +4,7 @@ This folder includes some utils for the project realization and documentation.
 All the support scripts used during the implementation are contained in the ```script``` folder.
 
 * The ```wakeup.py``` script is responsible to connect to pepper and let him wake up from a standby position.
+* The ```noobsetup.sh``` is a bash script to make the easiest setup possible to run the demo.
 ____
-
+ 
 ##### Group 18
---
+++ utils/script/noobsetup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo Hi noob, you are running $0
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install -y ros-melodic-octomap ros-melodic-octomap-msgs ros-melodic-rgbd-launch ros-melodic-camera-info-manager-py
+cd ~ # Go to the home directory
+wget https://community-static.aldebaran.com/resources/2.5.10/Python%20SDK/pynaoqi-python2.7-2.5.7.1-linux64.tar.gz
+tar xf pynaoqi-python2.7-2.5.7.1-linux64.tar.gz
+rm pynaoqi-python2.7-2.5.7.1-linux64.tar.gz
+D=$(realpath pynaoqi-python2.7-2.5.7.1-linux64)
+echo $D
+
+cd ~/ContestCognitiveRobotics2020 && catkin build
+cd ~/ContestCognitiveRobotics2020 && echo 'export PYTHONPATH=${PYTHONPATH}:$D/lib/python2.7/site-packages' >> devel/setup.bash
+cd ~/ContestCognitiveRobotics2020 && echo 'export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:$D/lib' >> devel/setup.bash
+cd ~/ContestCognitiveRobotics2020 && source devel/setup.bash
+echo "You're done !"
